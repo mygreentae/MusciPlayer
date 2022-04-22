ba890797a1bed8e673c384c1152f8d97c22f78bc
jacksonhorizon <jawico6@gmail.com>
2022-04-21 17:33:31 -0700

    Junit+Song javadoc

---
+++ src/utilities/Tester.java
@@ -0,0 +1,47 @@
+package utilities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import song.Song;
+
+
+public class Tester {
+
+	
+	/**
+	 * Tests 1-whatever test the functionality of the Song class
+	 */
+	@Test
+	void test1() {
+		// creates song, asserts everything works
+		Song song = new Song("yeet", "Seth");
+		assertFalse(song.isPlaying());
+		assertEquals(song.getName(), "yeet");
+		assertEquals(song.getArtist(), "Seth");
+		
+		// plays song
+		song.play();
+		song.setPlaying();
+		assertTrue(song.isPlaying());
+		
+		
+		// stops song
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		song.stop();
+		song.notPlaying();
+		assertFalse(song.isPlaying());
+	}
+	
+	
+}
