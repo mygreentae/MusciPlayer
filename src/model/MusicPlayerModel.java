d9f8c68f599ebe2d2cdb3c4293665d4f7c61d98a
jacksonhorizon <jawico6@gmail.com>
2022-04-21 18:55:49 -0700

    Stuff

---
+++ src/model/MusicPlayerModel.java
@@ -0,0 +1,44 @@
+package model;
+
+import song.Song;
+
+public class MusicPlayerModel {
+
+	private Song curSong; 
+	
+	private Song[] songs; 
+	
+	private int numSongs; 
+	
+	// add currentQueue
+	// add currentPlaylists
+	// add favorites
+	
+	
+	public MusicPlayerModel(Song start) {
+		curSong = start; 
+		songs = new Song[10];
+		songs[0] = start; 
+	}
+	
+	public void play() { // needs to be more rigorous 
+		curSong.setPlaying();
+		curSong.play();
+	}
+	
+	public void stop() {
+		curSong.stop();
+		curSong.notPlaying();
+	}
+	
+	public void addSong(Song toAdd) {
+		songs[numSongs] = toAdd; 
+		numSongs++;
+	}
+	
+	public void next() {
+		
+	}
+	
+	
+}
---
+++ src/utilities/Tester.java
@@ -0,0 +1,57 @@
+package utilities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import song.Song;
+
+
+public class Tester {
+
+	
+	/**
+	 * Tests 1 tests the functionality of the Song class
+	 */
+	@Test
+	void test1() {
+		// creates song, asserts everything works
+		Song song = new Song("yeet", "Seth", "the best");
+		assertFalse(song.isPlaying());
+		assertEquals(song.getName(), "yeet");
+		assertEquals(song.getArtist(), "Seth");
+		assertEquals(song.getGenre(), "the best");
+		// plays song
+		song.play();
+		song.setPlaying();
+		assertTrue(song.isPlaying());
+		
+		// stops song
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		song.stop();
+		song.notPlaying();
+		assertFalse(song.isPlaying());
+		
+		// checks favorite
+		assertFalse(song.isFavorite());
+		song.makeFavorite();
+		assertTrue(song.isFavorite());
+		song.unFavorite();
+		assertFalse(song.isFavorite());
+	
+	}
+	
+	@Test
+	void test2() {
+		Song song = new Song("yeet", "Seth", "the best");
+	}	
+}
